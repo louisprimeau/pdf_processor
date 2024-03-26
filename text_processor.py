2662d7a9d77ce8a16871e25f982864f069caf174
Louis Primeau <lprimeau@vols.utk.edu>
2024-03-26 09:16:54 -0400

    basic processing funcitonality

---
+++ text_processor.py
@@ -0,0 +1,23 @@
+
+import re
+from unidecode import unidecode
+
+# remove whitespace inside math mode expressions
+def format_math_whitespace(string):
+    substrings = string.split('$')
+    start = 1 if string[0] != '$' else 0
+    for i in range(start, len(substrings), 2):
+        substrings[i] = substrings[i].replace(' ', '')
+    return '$'.join(substrings)
+
+# replace hyphen spaces outside of math mode
+def replace_hyphen_spaces(string):
+    substrings = string.split('$')
+    start = 1 if string[0] == '$' else 0
+    for i in range(start, len(substrings), 2):
+        substrings[i] = re.sub('- (\s*)', '', substrings[i])
+    return '$'.join(substrings)
+
+# replace lookalike unicode to ascii
+def replace_common_unicode(string):
+    return unidecode(string, errors='ignore')
