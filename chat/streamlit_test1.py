f1272e02bd2cc90232cd5211c543c96f8e9fe7cd
Louis Primeau <lprimeau@vols.utk.edu>
2024-08-21 14:45:07 -0400

    barebones doc chat app

--- chat/streamlit_test1.py
+++ chat/streamlit_test1.py
@@ -1,57 +1,80 @@
 import streamlit as st
 import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
+import torch
+import json, sys, os
 
-sys_prompt = """You are an assistant for answering questions. You are given the extracted parts of a long document and a question. Don't make up an answer."""
-temp_question = "What is the critical temperature at zero-field of {MATERIAL}? Just give a number and do not provide any explanation. Format the answer as CRITICAL TEMPERATURE: {Number} K. Do not write a sentence before the output. Here are some example outputs: 'CRITICAL TEMPERATURE: 3 K', 'CRITICAL TEMPERATURE: 15.6 K'"
-source_question = "Can you reproduce the exact sentence where you got this information? Do not make up a sentence or attempt to explain."
+sys_prompt = """You are an assistant for answering questions. You are given the extracted parts of a long document and a question. Don't make up an answer. Here is the document: """
 
-def format_prompt(prompt, paper_text):
-  PROMPT = f"Question: {prompt}\nContext: " + paper_text
-  return PROMPT
+@st.cache_resource()
+def load_model():
+  model_sig='8B'
+  model_id = "meta-llama/Meta-Llama-3.1-{}-Instruct".format(model_sig)
 
-model_sig='8B'
-model_id = "meta-llama/Meta-Llama-3.1-{}-Instruct".format(model_sig)
-
-bnb_config = BitsAndBytesConfig(
-      load_in_4bit=True,
-      bnb_4bit_use_double_quant=True,
-      bnb_4bit_quant_type="nf4",
-      bnb_4bit_compute_dtype=torch.bfloat16
+  bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
   )
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+  tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-model = AutoModelForCausalLM.from_pretrained(
+  model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype=torch.bfloat16,
     quantization_config=bnb_config,
     device_map="auto",
-)
+  )
 
-terminators = [
+  terminators = [
     tokenizer.eos_token_id,
     tokenizer.convert_tokens_to_ids("<|eot_id|>")
-]
+  ]
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, torch_dtype=torch.bfloat16, device_map="auto", eos_token_id=terminators)
+  pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, torch_dtype=torch.bfloat16, device_map="auto", eos_token_id=terminators)
+
+  return pipe
 
 st.title("📝 File Q&A with LLama3.1-8B")
 
-uploaded_file = st.file_uploader("Upload an article", type=("txt"))
-question = st.text_input(
-    "Ask something about the article",
-    placeholder=temp_question,
-    disabled=not uploaded_file,
-)
+if "model" not in st.session_state:
+  st.session_state["model"] = load_model()
 
-if uploaded_file and question:
-    article = uploaded_file.read().decode()
+st.session_state.file_name = st.file_uploader("Upload an article", type=("txt"))
+  
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+  
+if "messages" in st.session_state and st.session_state.file_name is not None and len(st.session_state.messages) == 0:
+  print("initializing sys prompt")
+  st.session_state.messages = [{"role":"system", "content": sys_prompt + st.session_state.file_name.read().decode()[:18000]}]
 
-    messages = [{"role": "system", "content": sys_prompt},
-                {"role": "user", "content": format_prompt(question, article)}
-    
-    ]
-    response = pipe(messages, max_new_tokens=32)[0]['generated_text'][-1]['content']
+print("file_name" in st.session_state)
+print(st.session_state.file_name)
+print("messages" in st.session_state)
+  
+# Display chat messages from history on app rerun
+if "messages" in st.session_state:
+  print(st.session_state.messages)
+  print(st.session_state.file_name)
+  for message in st.session_state.messages:
+    if message["role"] != "system":
+      with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    st.write("### Answer")
-    st.write(response.completion)
+if "messages" in st.session_state:
+  if question := st.chat_input("ask a question"):
+    messages = st.session_state.messages
+    pipe = st.session_state["model"]
+
+    messages.append({"role": "user", "content": question})
+
+    with st.chat_message("user"):
+      st.markdown(question)
+
+    with st.chat_message("assistant"):
+      response = pipe(messages, max_new_tokens=64)[0]['generated_text'][-1]['content']
+      st.markdown(response)
+      
+    messages.append({"role": "assistant", "content": response})
