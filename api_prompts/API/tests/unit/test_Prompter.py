d3ea8328c3a4dd444186253413a61fa48c79f4a3
jackson-dendy <jdendy@vols.utk.edu>
2025-03-02 11:54:54 -0500

    Work on unit tests

--- api_prompts/API/tests/unit/test_Prompter.py
+++ api_prompts/API/tests/unit/test_Prompter.py
@@ -20,22 +20,27 @@ class Test_Prompter:
 
     def test_query(self):
         ob.query()
-        lst = jsonl_read(f"{root}//tests//unit//help_files//results//test_data_1//results.jsonl")
-        state = True
+        data = jsonl_read(f"{root}//tests//unit//help_files//results//test_data_1//results.jsonl")
+        state = False
         key_list = ["paper", "logs", "num", "chain", "prompt", "requests", "answer", "response", "LLM", "rouge2", "rougeL", "sentence_bool", "confidence", "sentence", "E2E"]
-        if len(lst) == 0:
-            state = False
+        if len(data) == 0:
+            assert state
         else:
-            for i in lst[0].keys():
+            for i in data[0].keys():
                 if i not in key_list:
-                    state = False
+                    assert state
                 if i == "logs":
-                    for j in i[0].keys():
+                    for j in data[0].i[0].keys():
                         if j not in key_list:
-                            state = False
+                            assert state
                         if j == "requests":
-                            for k in k[0].keys():
+                            for k in data.i[0].j[0].keys():
                                 if k not in key_list:
-                                    state = False
+                                    assert state
+                        elif i == "chain":
+                            for k in data[0].i[0].j[0].keys():
+                                if j not in key_list:
+                                    assert state
+            
         shutil.rmtree(f"{root}//tests//unit//help_files//results//test_data_1")
-        assert state
+        assert not(state)
